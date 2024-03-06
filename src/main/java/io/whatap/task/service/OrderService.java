5c7a5bd9982da272d8bac7c0cdd0221f8c8c4a88
dduneon <dduneon@gmail.com>
2024-03-07 01:49:33 +0900

    ADD: Resource, Service 클래스 추가

---
+++ src/main/java/io/whatap/task/service/OrderService.java
@@ -0,0 +1,13 @@
+package io.whatap.task.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+/**
+ * description
+ *
+ * @author 김준현
+ * @version 2024. 03. 07
+ */
+@ApplicationScoped
+public class OrderService {
+}
