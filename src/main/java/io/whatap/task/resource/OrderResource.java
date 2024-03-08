8c50497f42c68f3e950bd7775f9f8cd8fa60dc21
dduneon <dduneon@gmail.com>
2024-03-09 03:17:14 +0900

    FEAT: Controller Method 추가

--- src/main/java/io/whatap/task/resource/OrderResource.java
+++ src/main/java/io/whatap/task/resource/OrderResource.java
@@ -1,7 +1,13 @@
 package io.whatap.task.resource;
 
+import io.whatap.task.domain.req.OrderCreateRequestDto;
+import io.whatap.task.domain.req.OrderUpdateRequestDto;
 import io.whatap.task.service.OrderService;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
@@ -31,6 +37,25 @@ public class OrderResource {
     @GET
     @Path("/{orderId}")
     public Response getOrder(@PathParam("orderId") Long orderId) {
-        return Response.ok(orderService.readAllOrders()).build();
+        return Response.ok(orderService.readDetailByOrderId(orderId)).build();
+    }
+
+    @POST
+    public Response orderProduct(@Valid OrderCreateRequestDto orderCreateRequestDto) {
+        orderService.createOrder(orderCreateRequestDto);
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @PUT
+    public Response changeProduct(@Valid OrderUpdateRequestDto orderUpdateRequestDto) {
+        orderService.updateOrder(orderUpdateRequestDto);
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
+
+    @DELETE
+    @Path("/{orderId}")
+    public Response deleteOrder(@PathParam("orderId") Long orderId) {
+        orderService.deleteOrder(orderId);
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
 }
