ce38a199149a83833f7609ac13551a9644d46b2c
dduneon <dduneon@gmail.com>
2024-03-09 18:11:42 +0900

    FEAT: Validation 옵션 추가

--- src/main/java/io/whatap/task/domain/req/OrderCreateRequestDto.java
+++ src/main/java/io/whatap/task/domain/req/OrderCreateRequestDto.java
@@ -1,5 +1,6 @@
 package io.whatap.task.domain.req;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,5 +13,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class OrderCreateRequestDto {
+    @NotBlank(message = "productId 필드가 비어 있습니다")
     private Long productId;
 }
--- src/main/java/io/whatap/task/domain/req/OrderUpdateRequestDto.java
+++ src/main/java/io/whatap/task/domain/req/OrderUpdateRequestDto.java
@@ -1,5 +1,6 @@
 package io.whatap.task.domain.req;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class OrderUpdateRequestDto {
+    @NotBlank(message = "orderId 필드가 비어 있습니다")
     private Long orderId;
+    @NotBlank(message = "productId 필드가 비어 있습니다")
     private Long productId;
 }
